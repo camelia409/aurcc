5716eea6a10cee94e91d9b691e243c0f50c7a31d
Janarthanan-Gnanamurthy <janagnanam12@gmail.com>
2024-06-22 23:01:35 +0530

    misc

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,11 @@
   <div class="min-h-screen bg-base-200">
     <main class="flex flex-col">
 
-      <div class=" flex flex-col gap-4 lg:w-1/2 self-center">
+      <!-- <div class=" flex flex-col gap-4 lg:w-1/2 self-center">
         <div class="text-center">
             <h1 class="text-3xl font-bold p-4" style="font-family: Clash Grotesk;">Counselling Code: 2025</h1>
         </div>
-      </div>
+      </div> -->
       
 
 
