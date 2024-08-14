c7073ec26e9d27c5ce22fc5f5d06b7020200389c
spyking05 <ritikrd05@gmail.com>
2024-08-14 15:31:03 +0530

    adminstration,admin staffs

--- src/views/EDcell.vue
+++ src/views/EDcell.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="flex-grow">
     <!-- Hero section -->
-    <section class="bg-cover bg-center relative h-64" :style="'background-image: url(http://192.168.72.231:5173/src/assets/dgate-hero.jpg)'">
+    <section class="bg-cover bg-center relative -z-10" :style="'background-image: url(http://192.168.72.231:5173/src/assets/dgate-hero.jpg)'">
       <div class="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
       <div class="container mx-auto py-16 text-white p-9 relative z-10">
         <h1 class="text-4xl font-bold mb-4">ED Cell</h1>
