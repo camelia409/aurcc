08480997bf2a25ad860e633ebbc962c5a6d4bcc5
Subramaniyasiva002 <sssubramaniyasiva@gmail.com>
2024-09-08 23:52:48 +0530

    rithik-offices

--- src/views/PlacementCell.vue
+++ src/views/PlacementCell.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex-grow">
     <!-- Hero Section -->
-    <section class="bg-cover bg-center relative -z-10" :style="'background-image: url(http://192.168.72.231:5173/src/assets/dgate-hero.jpg)'">
-      <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-blue-900 via-transparent to-blue-900 opacity-70"></div>
+    <section 
+    class="hero-section bg-cover  py-32 relative -z-10" 
+    :style="{ 
+     backgroundImage: `url(${backgroundImage})`, 
+      backgroundSize: 'cover',
+      backgroundPosition: 'top center' 
+    }">
+<!--      <div class="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-blue-900 via-transparent to-blue-900 opacity-70"></div>
       <div class="container mx-auto py-10 text-center relative z-10 px-4">
         <h1 class="text-4xl font-extrabold text-black drop-shadow-lg font-serif">Placement Cell</h1>
-      </div>
+      </div>-->
     </section>
 
     <!-- Content Sections with Vertical Tabs -->
@@ -29,27 +35,27 @@
         <!-- Content -->
         <div class="w-full px-10 font-serif ">
           <div v-if="currentSection === 'Description'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">Description</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">Description</h2>
             <p class="text-base md:text-lg mb-8 text-black">{{ data.description }}</p>
           </div>
           <div v-else-if="currentSection === 'Recruitment Process'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">Recruitment Process</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">Recruitment Process</h2>
             <p class="text-base md:text-lg mb-8 text-black">{{ data.recruitment }}</p>
           </div>
           <div v-else-if="currentSection === 'CUIC'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">CUIC</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">CUIC</h2>
             <p class="text-base md:text-lg mb-8 text-black">{{ data.cuic }}</p>
           </div>
           <div v-else-if="currentSection === 'Our Recruiters'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">Our Recruiters</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">Our Recruiters</h2>
             <iframe :src="recruitersPDF" class="w-full h-[700px] rounded-md shadow-lg" frameborder="0"></iframe>
           </div>
           <div v-else-if="currentSection === 'Placed Students'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">Placed Students</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">Placed Students</h2>
             <img :src="placedStudentsImage" alt="Placed Students" class="w-full h-auto rounded-md shadow-lg" />
           </div>
           <div v-else-if="currentSection === 'Staff Members'">
-            <h2 class="text-2xl md:text-4xl text-black font-bold mb-4 mt-5">Staff Members</h2>
+            <h2 class="text-2xl md:text-4xl font-bold mb-4 mt-5">Staff Members</h2>
             <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
               <div v-for="member in data.staff_members" :key="member.emailid" class="bg-gray-200 shadow-md text-xl rounded-md p-4">
                 <h3 class="font-semibold">{{ member.name }}</h3>
@@ -68,11 +74,14 @@
 import data from '../assets/placement-cell.json';
 import recruitersPDF from '../assets/placementBrochure.pdf';
 import placedStudentsImage from '../assets/Placedstudents.webp';
+import backgroundImage from '@/assets/placement.webp';
+
 
 export default {
   data() {
     return {
       data: data,
+      backgroundImage:backgroundImage,
       currentSection: 'Recruitment Process', // Default to first section
       recruitersPDF,
       placedStudentsImage,
